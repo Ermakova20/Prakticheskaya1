72f60a3726c4934fc2a975d4ea44198877ec3965
Ermakova20 <oksanasedko123@gmail.com>
2020-03-24 14:22:20 +0300

    3 commit

---
+++ Prakticheskaya1/Obyv.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prakticheskaya1
+{
+    class Obyv
+    {
+        public string proizvoditel { get; set; }
+        public int razmer { get; set; }
+        public double cena { get; set; }
+    }
+}
